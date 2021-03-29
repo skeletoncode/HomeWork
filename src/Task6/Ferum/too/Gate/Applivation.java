bfd09479f895561b5f20fb0f493b9ff5102fb9c2
code <romacomcom@gmail.com>
2021-03-29 15:14:49 +0300

    change  29.03.2021 test version 1.0

---
+++ src/Task6/Ferum/too/Gate/Applivation.java
@@ -0,0 +1,21 @@
+package Task6.Ferum.too.Gate;
+
+public class Applivation {
+    public static void main(String[] args) {
+        // Repository Repository1 = new Repository();
+      //  Repository Repository2 = new Repository();
+// не понятно в чем ошибка или ошибки..
+
+         Author Author1 = new Author();
+         SetAuthorName(" ighgb");
+         Book Book1 = new Book();
+         Set
+         SetBookName("Мифы");
+
+
+       // System.out.println(Repository1);
+       // System.out.println(Repository2);
+    }
+
+
+}
