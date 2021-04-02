95e5a32bb94167aae63c07c7b234836e968931b5
code <romacomcom@gmail.com>
2021-04-02 23:53:19 +0300

    change  02.04.2021 test version 1.0

--- src/Task6/Ferum/too/Gate/Task6muse/AppMouse.java
+++ src/Task6/Ferum/too/Gate/Task6muse/AppMouse.java
@@ -6,14 +6,17 @@ public class AppMouse {
         Mouse mouse1 = new Mouse(55);
         Mouse mouse2 = new Mouse(68);
         Mouse mouse3 = new Mouse( 75);
+      //  Mouse [] mouseX = new Mouse {mouse1, mouse2,mouse3};
 
 
+        Cat cat1 = new Cat("wef","8",80, 67, new Mouse[]{mouse1, mouse2, mouse3, null, null} );
+        Cat cat2 = new Cat("vbv","8",90, 57, new Mouse[]{mouse1, mouse2, null, null, null} );
+        cat1.catchCat(cat2);
 
-        Cat cat1 = new Cat("wef","8",80, 67, );
-        Cat cat2 = new Cat();
+       // Cat cat2 = new Cat();
 
-  /*
-  public void addMouse(Mouse mouse)    --- нужна ли эта строчка..?
+
+  /*public void catchCat(Cat cattt)    --- нужна ли эта строчка..?
   if (cat1.weight > cat2.weight){
 
             for (int i = 0; i < cat2.mousesCatch.length, i ++) {
